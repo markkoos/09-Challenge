3c5c1d8781bd6453130fdf6b30bf4e008cc955e2
markkoos <markusimercado@gmail.com>
2022-08-30 18:36:32 -0700

    added license options to inquirer questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
+const generateMarkdown = require(`/g/utils/generateMarkdown.js`)
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -32,6 +33,43 @@ const questions = [
         type: `input`,
         message: `Enter test instructions.`,
         name: `tests`,
+    },
+    {
+        type: `list`,
+        message: `Choose a license for your application.`,
+        name: `license`,
+        choices: [
+            {
+                name: `Apache 2.0`,
+                value: `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+            },
+            {
+                name: `Eclipse Public License 1.0`,
+                value: `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`,
+            },
+            {
+                name: `Do What the F*** You Want to Public License`,
+                value: `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`,
+            },
+            {
+                name: `The MIT License`,
+                value: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+            },
+            {
+                name: `Mozilla Public License 2.0`,
+                value: `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+            }
+        ]
+    },
+    {
+        type: `input`,
+        message: `Enter your GitHub username.`,
+        name: `username`,
+    },
+    {
+        type: `input`,
+        message: `Enter your email address.`,
+        name: `email`,
     },
 ];
 
